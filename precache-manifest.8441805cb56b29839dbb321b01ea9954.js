34bce47f82f2b34fa648417636d32d8bb7950d16
rockingatgithub <contactwithsid@gmail.com>
2020-08-25 14:14:35 +0530

    Updates

---
+++ precache-manifest.8441805cb56b29839dbb321b01ea9954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed50dedd7155bfd2f0791025ab1be9e7",
+    "url": "/React_Shop/index.html"
+  },
+  {
+    "revision": "12e5614aeb7a9e16553d",
+    "url": "/React_Shop/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ffb389999a60f2ad1b0d",
+    "url": "/React_Shop/static/css/main.260247aa.chunk.css"
+  },
+  {
+    "revision": "12e5614aeb7a9e16553d",
+    "url": "/React_Shop/static/js/2.dd3b9c2a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Shop/static/js/2.dd3b9c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffb389999a60f2ad1b0d",
+    "url": "/React_Shop/static/js/main.d1fcab90.chunk.js"
+  },
+  {
+    "revision": "183f594600732aa94ff0",
+    "url": "/React_Shop/static/js/runtime-main.19a54f6e.js"
+  }
+]);
